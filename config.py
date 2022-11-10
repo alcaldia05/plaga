3409a8da2afa93edbd1aa2e4afd5f1a46bbb8bb1
antoj15 <99308145+antoj15@users.noreply.github.com>
2022-11-10 10:12:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ API_ID =  os.environ.get('api_id','15453386')
 API_HASH = os.environ.get('api_hash','5589571436:AAHKPwzyNIISViNs39oaICkYL_595dBT618')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','EL_Wizard').split(';')
-#ACCES_USERS = ('tl_admin_user','EL_Wizard','Stvz20')
+ACCES_USERS = os.environ.get('tl_admin_user','toni8790').split(';')
+#ACCES_USERS = ('tl_admin_user','edel1989','Gavi_04Arias')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDHKDYDJHJHGIYJJHGGGDYJIDIDRKGILKGGKG'))
 
