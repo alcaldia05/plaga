2e81376e067f13fafa4bf0ecba1341ac11a6b92d
antoj15 <99308145+antoj15@users.noreply.github.com>
2022-11-10 10:14:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5589571436:AAHKPwzyNIISViNs39oaICkYL_595dBT618')
-API_ID =  os.environ.get('api_id','15453386')
-API_HASH = os.environ.get('api_hash','5589571436:AAHKPwzyNIISViNs39oaICkYL_595dBT618')
+BOT_TOKEN =  os.environ.get('bot_token','5593162832:AAFnt3oA20cZ5P4cB-RajHm-nu4h1x3NEKc')
+API_ID =  os.environ.get('api_id','17232014')
+API_HASH = os.environ.get('api_hash','5eb747e0b34842c90a32af2df2364e32')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','toni8790').split(';')
