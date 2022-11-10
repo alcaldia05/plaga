9c780b62b2b6c2f47d76efd9a69c46ae87a1a169
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-11-10 11:10:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5777043254:AAGky5P7SMDNfhpET7rtxY8qAhi-mUwA_VI')
-API_ID =  os.environ.get('api_id','17232014')
-API_HASH = os.environ.get('api_hash','5eb747e0b34842c90a32af2df2364e32')
+BOT_TOKEN =  os.environ.get('bot_token','5374066017:AAF3yWdq_c0_kFz0utLb7joE6B1QM5UtUk4')
+API_ID =  os.environ.get('api_id','17158127')
+API_HASH = os.environ.get('api_hash','d1a5974070430293b64fde5339591447')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','toni8790').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
 #ACCES_USERS = ('tl_admin_user','toni8790','Gavi_04Arias')
 #ACCES_USERS = os.environ.get(ACCES_USERS)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDHKDYDJHJHGIYJJHGGGDYJIDIDRKGILKGGKG'))
